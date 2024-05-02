9ce8bbbe5841e6922a11ead1c842997480beda19
p1r473 <subwayjared@gmail.com>
2024-05-02 15:16:02 -0400

    Add variable so it doesnt get loaded at shell startup
    This is so it doesnt immediately ls if you have a cd in your .zshrc file
    add to end of .zshrc  `CD_LS_INIT_COMPLETE=true
    zstyle ':hist:*' load-hooks false`
    This is a work around for https://github.com/zdharma-continuum/fast-syntax-highlighting/issues/65 as they were conflicting
    Is there a better way to fix this?

--- cd-ls.plugin.zsh
+++ cd-ls.plugin.zsh
@@ -1,10 +1,16 @@
+zstyle ':cd-ls:*' cd_ls_init_complete= false
+
 # 'ls' after every 'cd'
 if ! (( $chpwd_functions[(I)chpwd_cdls] )); then
   chpwd_functions+=(chpwd_cdls)
 fi
 function chpwd_cdls() {
+  if if zstyle -T ':hist:*' load-hooks; then
+    return
+  fi
   if [[ -o interactive ]]; then
     emulate -L zsh
     eval ${CD_LS_COMMAND:-ls}
   fi
 }
+
